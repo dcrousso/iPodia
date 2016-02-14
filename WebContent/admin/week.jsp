a0ba0f610f440327bfe0aa55b483b2e9e0a127bf
Devin Rousso <dcrousso@gmail.com>
2016-02-13 18:49:10 -0800

    Reformatted some of the code for file upload

--- WebContent/admin/week.jsp
+++ WebContent/admin/week.jsp
@@ -1,25 +1,25 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
-<!-- http://stackoverflow.com/questions/19510656/how-to-upload-files-on-server-folder-using-jsp -->
+<%@ include file="/WEB-INF/Session.jsp" %>
+<%
+if (!user.isAuthenticated() || !user.isAdmin()) {
+	response.sendRedirect(request.getContextPath() + "/");
+	return;
+}
+%>
+<jsp:include page="/WEB-INF/templates/head.jsp">
+	<jsp:param name="pagetype" value="student"/>
+	<jsp:param name="title" value="Student"/>
+</jsp:include>
+<jsp:include page="/WEB-INF/templates/header.jsp">
+	<jsp:param name="username" value="${user.getName()}"/>
+</jsp:include>
+		<main>
+			<form method="post" action="uploadFile" enctype="multipart/form-data">
+				<input type="text" name="className" value="${param.className}" hidden>
+				<input type="text" name="week" value="${param.week}" hidden>
 
-</head>
-<body>
-	<form method="post" action="uploadFile" enctype="multipart/form-data">
-		<%-- <input type="text" name="class" value="${param.className}" hidden>
-		<input type="text" name="week" value="${param.week}" hidden> --%>
+				<input type="file" name="fileToUpload">
 
-		<div> Select file to upload: </div>
-	    <input type="file" name="fileToUpload" id="fileToUpload">
-
-		<input type="submit" value="File Upload">
-	</form>
-	
-
-
-</body>
-</html>
+				<input type="submit" value="Submit">
+			</form>
+		</main>
+<jsp:include page="/WEB-INF/templates/footer.jsp"/>
--- src/iPodia/FileUploadServlet.java
+++ src/iPodia/FileUploadServlet.java
@@ -2,8 +2,6 @@ package iPodia;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,91 +13,66 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-/**
- * Servlet implementation class UploadServlet
- */
 public class FileUploadServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static final String DATA_DIRECTORY = "/iPodiaData";
-   //  private static final String DATA_DIRECTORY = "/Users/AlexKoh/Desktop/workspace/iPodia/data";
-    private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 2;
-    private static final int MAX_REQUEST_SIZE = 1024 * 1024;
+	private static final int MAX_MEMORY_SIZE = 1024 * 1024 * 10; // 10MB
+	private static final int MAX_REQUEST_SIZE = 1024 * 1024; // 1MB
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String className = request.getParameter("className");
+		String week = request.getParameter("week");
+		System.out.println(className);
+		System.out.println(week);
+		if (className == null || week == null) {
+			getServletContext().getRequestDispatcher("/").forward(request, response);
+			return;
+		}
 
-        // Check that we have a file upload request
-        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+		// Check that we have a file upload request
+		if (!ServletFileUpload.isMultipartContent(request))
+			return;
 
-        if (!isMultipart) {
-            return;
-        }
+		// Create a factory for disk-based file items
+		DiskFileItemFactory factory = new DiskFileItemFactory();
 
-        // Create a factory for disk-based file items
-        DiskFileItemFactory factory = new DiskFileItemFactory();
+		// Sets the size threshold beyond which files are written directly to disk
+		factory.setSizeThreshold(MAX_MEMORY_SIZE);
 
-        // Sets the size threshold beyond which files are written directly to
-        // disk.
-        factory.setSizeThreshold(MAX_MEMORY_SIZE);
+		// Sets the directory used to temporarily store files that are larger than
+		// the configured size threshold. We use temporary directory for java
+		factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
 
-        // Sets the directory used to temporarily store files that are larger
-        // than the configured size threshold. We use temporary directory for
-        // java
-        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+		File dataFolder = new File(Defaults.DATA_DIRECTORY);
+		if (!dataFolder.exists() || !dataFolder.isDirectory())
+			dataFolder.mkdir();
 
-        // constructs the folder where uploaded file will be stored
-       // String uploadFolder = getServletContext().getRealPath("") + DATA_DIRECTORY;
-        
-       // String uploadFolder = request.toString() + DATA_DIRECTORY;
-       // System.out.println(uploadFolder);
+		// Create a new file upload handler
+		ServletFileUpload upload = new ServletFileUpload(factory);
 
-        File dataFolder = new File(DATA_DIRECTORY);
-        if (!dataFolder.exists() || !dataFolder.isDirectory()) {
-        	dataFolder.mkdir();
-        }
-        
-        
+		// Set overall request size constraint
+		upload.setSizeMax(MAX_REQUEST_SIZE);
 
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload(factory);
+		try {
+			for (FileItem item : upload.parseRequest(request)) {
+				if (item.isFormField())
+					continue;
 
-        // Set overall request size constraint
-        upload.setSizeMax(MAX_REQUEST_SIZE);
-        
-        System.out.println("hi");
-        try {
-            // Parse the request
-            List items = upload.parseRequest(request);
-            Iterator iter = items.iterator();
-            while (iter.hasNext()) {
-                FileItem item = (FileItem) iter.next();
+				String filePath = Defaults.DATA_DIRECTORY + File.separator + item.getName();
+				File uploadedFile = new File(filePath);
+				if (!uploadedFile.exists())
+					uploadedFile.createNewFile();
 
-                if (!item.isFormField()) {
-                    String fileName = new File(item.getName()).getName();
-                    System.out.println("filename: " +fileName);
-                    String filePath = DATA_DIRECTORY + File.separator + fileName;
-                    System.out.println("file path: " +filePath);
-                    File uploadedFile = new File(filePath);
-                    System.out.println(filePath);
-                    if (!uploadedFile.exists()) {
-                    	uploadedFile.createNewFile();
-                    	
-                    }
-                   
-                    item.write(uploadedFile);
-                }
-            }
+				item.write(uploadedFile);
+			}
 
-            // displays done.jsp page after upload finished
-            //getServletContext().getRequestDispatcher("/week.jsp?className=").forward(
-              //      request, response);
+		} catch (FileUploadException ex) {
+			throw new ServletException(ex);
+		} catch (Exception ex) {
+			throw new ServletException(ex);
+		}
 
-        } catch (FileUploadException ex) {
-            throw new ServletException(ex);
-        } catch (Exception ex) {
-            throw new ServletException(ex);
-        }
-
-    }
-
+		String redirectURL = "/week.jsp?className=" + className +"&week=" + week;
+		getServletContext().getRequestDispatcher(redirectURL).forward(request, response);
+	}
 }
